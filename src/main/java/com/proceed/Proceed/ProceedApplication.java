8191bbaf261c7b37918f9f77f4e7eb2401bac870
koo9b9h <rnqudgur@gmail.com>
2023-01-15 14:13:28 +0900

    create project
    프로젝트 생성 이제부터 시작

---
+++ src/main/java/com/proceed/Proceed/ProceedApplication.java
@@ -0,0 +1,13 @@
+package com.proceed.Proceed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProceedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProceedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proceed/Proceed/ProceedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proceed.Proceed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProceedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
